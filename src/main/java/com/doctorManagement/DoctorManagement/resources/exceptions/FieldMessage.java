bc3f846c8f55ba2c799e61eeca33c8e8e6ed3eaf
ccaiomacedo <caio.macedo@ucsal.edu.br>
2021-09-18 02:04:56 -0300

    Creating Exception Classes for Unfound Requests

---
+++ src/main/java/com/doctorManagement/DoctorManagement/resources/exceptions/FieldMessage.java
@@ -0,0 +1,35 @@
+package com.doctorManagement.DoctorManagement.resources.exceptions;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+    private static final long serialVersionUID=1L;
+
+    private String nome;
+    private String message;
+
+    public FieldMessage(){
+
+    }
+
+    public FieldMessage(String nome, String message) {
+        this.nome = nome;
+        this.message = message;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
